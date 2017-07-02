3eede2c728d3f50f5c3e0478dcb623a69d7a16cb
Quentin Monmert <qmonmert@mbp-de-cedric.home>
2017-07-02 18:22:36 +0200

    Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { ImgAnimationComponent } from './img-animation/img-animation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgAnimationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
